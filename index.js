96accef03b201bb802c164c54ee795502af7974a
Dan Park <danieljinwoopark@gmail.com>
2017-12-19 08:48:09 -0600

    promises work now

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
 const Gdax = require('gdax');
 
 
-const publicClient = new Gdax.PublicClient();
+const BTCUSD_publicClient = new Gdax.PublicClient('BTC-USD');
+const LTCUSD_publicClient = new Gdax.PublicClient('LTC-USD');
+const LTCBTC_publicClient = new Gdax.PublicClient('LTC-BTC');
+
 const orderbook = new Gdax.Orderbook();
 
-const callback = function(err, response, data) {
-    if(err) return console.error(err)
-    // console.log(data.bids);
-    something(data.bids);
-};
 
-const something = function(data){
-// takes an array of arrays
-// i want to see the one id with the largest amount of orders in the books
-    let data2 = [[1, 2, 'dan'],[1, 23, 'dan'],
-                [2, 24, 'dan'],[1, 2, 'jim'],
-                [3, 34, 'jim'],[1, 2, 'fax'],]
+// publicClient.getProducts(callback);
+const bar = async function(){
+    let foo = await BTCUSD_publicClient.getProductOrderBook({'level':1});
+    console.log(foo);
+}
 
-    let dan = data.reduce((a, b) =>  {
-        const [price,size,id] = b;
-        a[id] = a[id] ? a[id] + 1 : 1
-        return a;
-    }, {})
-
-    let filterObj = Object.keys(dan).reduce((a, b) => {
-        console.log(b);
-        if (dan[b] > 1){
-            console.log(b, dan[b]);
-        }
-        return a;
-    }, {})
-
-    console.log(filterObj);
-};
-
-//publicClient.getProducts(callback)
-publicClient.getProductOrderBook({'level':3}, callback)
+bar();
+// look at ltc/btc v the spread
+// figure out which bid/ask to look at
